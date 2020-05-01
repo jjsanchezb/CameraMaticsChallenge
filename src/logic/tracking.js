aaffb031a71216f30adacbef3f041c0846eb70bc
Jhon Jairo Sanchez Benavides <j.jairosanchez.b@gmail.com>
2020-05-01 08:28:01 -0500

    static files for tracking

--- src/logic/tracking.js
+++ src/logic/tracking.js
@@ -1,11 +1,8 @@
 import jbinary from "jbinary";
 const getTrackingData = async function() {
-  jbinary.load(
-    "http://localhost:8080/data/track_2020-04-23T14-25-30.bin",
-    function(err, binary) {
-      console.log(binary.read({ header: ["string", 177] }));
-    }
-  );
+  jbinary.load("/data/track_2020-04-23T14-25-30.bin", function(err, binary) {
+    console.log(binary.read({ header: ["string", 177] }));
+  });
 };
 export default {
   getTrackingData
